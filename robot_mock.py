3ca12c947c2c2cab54e89e71084e8eb5d5d7edf3
kobivbe21 <156514292+Kobi4041@users.noreply.github.com>
2026-02-01 08:38:06 +0200

    Update robot_mock.py

--- robot_mock.py
+++ robot_mock.py
@@ -3,4 +3,5 @@ class RobotMock:
         print("Robot Mock Initialized - Waiting for commands...")
 
     def action(self, name):
-        print(f"[{name.upper()}] - Robot is performing the action.")
+
+        print(f"[{name.upper()}] - Robot is performing the action.")vhb
